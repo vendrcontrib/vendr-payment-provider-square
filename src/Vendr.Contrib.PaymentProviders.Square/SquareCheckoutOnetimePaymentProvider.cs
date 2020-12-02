0ef67a6c88a402b1de5f6de562999d04b306eca8
Paul Seal <prjseal@gmail.com>
2020-12-02 22:16:31 +0000

    - added square webhook model to httpcontext

--- src/Vendr.Contrib.PaymentProviders.Square/SquareCheckoutOnetimePaymentProvider.cs
+++ src/Vendr.Contrib.PaymentProviders.Square/SquareCheckoutOnetimePaymentProvider.cs
@@ -1,4 +1,4 @@
-using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 using Square.Models;
 using System;
 using System.Collections.Generic;
@@ -8,13 +8,13 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using Vendr.Contrib.PaymentProviders.Square.Models;
 using Vendr.Core;
 using Vendr.Core.Models;
 using Vendr.Core.Web;
 using Vendr.Core.Web.Api;
 using Vendr.Core.Web.PaymentProviders;
 using SquareSdk = Square;
-using Vendr.Core.Helpers;
 
 namespace Vendr.Contrib.PaymentProviders.Square
 {
@@ -38,19 +38,15 @@ namespace Vendr.Contrib.PaymentProviders.Square
             var orderApi = client.OrdersApi;
             SquareSdk.Models.Order squareOrder = null;
 
-            var isValidRequest = ValidateSquareSignature(request, settings);
+            var squareEvent = GetSquareWebhookEvent(request, settings);
 
-            if(isValidRequest)
+            if(squareEvent != null && squareEvent.IsValid)
             {
                 try
                 {
-                    var orderId = "";
                     var referenceId = "";
-                    if (request.InputStream.CanSeek)
-                        request.InputStream.Seek(0, SeekOrigin.Begin);
 
-                    orderId = GetOrderIdFromRequest(request);
-
+                    var orderId = GetOrderId(squareEvent);
                     if (!string.IsNullOrWhiteSpace(orderId))
                     {
                         var result = orderApi.BatchRetrieveOrders(
@@ -73,69 +69,6 @@ namespace Vendr.Contrib.PaymentProviders.Square
             }
 
             return base.GetOrderReference(request, settings);
-        }
-
-        private static bool ValidateSquareSignature(HttpRequestBase request, SquareSettings settings)
-        {
-
-            var body = GetRequestBody(request);
-            var url = request.Url.ToString();
-
-            var signatureKey = settings.SandboxMode ? settings.SandboxWebhookSigningSecret: settings.LiveWebhookSigningSecret;
-            var signature = request.Headers["x-square-signature"];
-
-            var combined = url + body;
-
-            var hmac = HMACSHA1(signatureKey, combined);
-            var checkHash = Base64Encode(hmac);
-
-            return !string.IsNullOrWhiteSpace(signature) && signature == checkHash;
-        }
-        
-        private static string GenerateHashString(HashAlgorithm algorithm, string input)
-        {
-            var bytes = algorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
-            return string.Join(string.Empty, bytes.Select(x => x.ToString("X2")));
-        }
-
-        protected static string HMACSHA1(string key, string input)
-        {
-            var result = default(string);
-            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
-            using (var algorithm = new HMACSHA1(keyBytes))
-            {
-                result = GenerateHashString(algorithm, input);
-            }
-            return result;
-        }
-
-        private static string GetRequestBody(HttpRequestBase request)
-        {
-            using (var reader = new StreamReader(request.InputStream))
-            {
-                var body = reader.ReadToEnd();
-                return body;
-            }
-        }
-
-        private static string GetOrderIdFromRequest(HttpRequestBase request)
-        {
-            string orderId;
-            using (var reader = new StreamReader(request.InputStream))
-            {
-                var json = reader.ReadToEnd();
-
-                if(string.IsNullOrWhiteSpace(json))
-                {
-                    return null;
-                }
-
-                var jsonObject = JObject.Parse(json);
-
-                orderId = jsonObject["data"]["object"]["payment"]["order_id"].ToString();
-            }
-
-            return orderId;
         }
 
         public override bool FinalizeAtContinueUrl => false;
@@ -222,9 +155,9 @@ namespace Vendr.Contrib.PaymentProviders.Square
             var accessToken = settings.SandboxMode ? settings.SandboxAccessToken : settings.LiveAccessToken;
             var environment = settings.SandboxMode ? SquareSdk.Environment.Sandbox : SquareSdk.Environment.Production;
 
-            var isValidRequest = ValidateSquareSignature(request, settings);
+            var squareEvent = GetSquareWebhookEvent(request, settings);
 
-            if(!isValidRequest)
+            if(squareEvent != null && squareEvent.IsValid)
             {
                 try
                 {
@@ -235,7 +168,7 @@ namespace Vendr.Contrib.PaymentProviders.Square
 
                     var orderApi = client.OrdersApi;
 
-                    var orderId = GetOrderIdFromRequest(request);
+                    var orderId = GetOrderId(squareEvent);
 
                     var paymentStatus = PaymentStatus.PendingExternalSystem;
                     SquareSdk.Models.Order squareOrder = null;
@@ -264,16 +197,13 @@ namespace Vendr.Contrib.PaymentProviders.Square
                         }
                     }
 
-                    return new CallbackResult
+                    return CallbackResult.Ok(new TransactionInfo
                     {
-                        TransactionInfo = new TransactionInfo
-                        {
-                            AmountAuthorized = order.TotalPrice.Value.WithTax,
-                            TransactionFee = 0m,
-                            TransactionId = Guid.NewGuid().ToString("N"),
-                            PaymentStatus = paymentStatus
-                        }
-                    };
+                        AmountAuthorized = order.TotalPrice.Value.WithTax,
+                        TransactionFee = 0m,
+                        TransactionId = squareOrder.Id,
+                        PaymentStatus = paymentStatus
+                    });
                 }
                 catch (Exception ex)
                 {
@@ -282,6 +212,79 @@ namespace Vendr.Contrib.PaymentProviders.Square
             }
 
             return CallbackResult.BadRequest();
+        }
+
+        protected SquareWebhookEvent GetSquareWebhookEvent(HttpRequestBase request, SquareSettings settings)
+        {
+            const string EventKey = "Vendr_SquareEvent";
+            SquareWebhookEvent squareEvent = null;
+
+            if(HttpContext.Current.Items[EventKey] != null)
+            {
+                squareEvent = (SquareWebhookEvent)HttpContext.Current.Items[EventKey];
+            }
+            else
+            {
+                try
+                {
+                    if (request.InputStream.CanSeek)
+                        request.InputStream.Seek(0, SeekOrigin.Begin);
+
+                    using (var reader = new StreamReader(request.InputStream))
+                    {
+                        var json = reader.ReadToEnd();
+
+                        var url = request.Url.ToString();
+                        var signature = request.Headers["x-square-signature"];
+
+                        squareEvent = JsonConvert.DeserializeObject<SquareWebhookEvent>(json);
+                        squareEvent.IsValid = ValidateSquareSignature(json, url, signature, settings);
+
+                        HttpContext.Current.Items[EventKey] = squareEvent;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Vendr.Log.Error<SquareCheckoutOnetimePaymentProvider>(ex, "Square - GetSquareWebhookEvent");
+                }
+            }
+
+            return squareEvent;
+        }
+
+        protected bool ValidateSquareSignature(string body, string url, string signature, SquareSettings settings)
+        {
+            var signatureKey = settings.SandboxMode ? settings.SandboxWebhookSigningSecret : settings.LiveWebhookSigningSecret;
+
+            var combined = url + body;
+
+            var hmac = HMACSHA1Hash(signatureKey, combined);
+            var checkHash = Base64Encode(hmac);
+
+            return !string.IsNullOrWhiteSpace(signature) && signature == checkHash;
+        }
+
+        protected byte[] HMACSHA1Hash(string key, string input)
+        {
+            byte[] result = new byte[0];
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+
+            using (var algorithm = new HMACSHA1(keyBytes))
+            {
+                result = algorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+            }
+
+            return result;
+        }
+
+        protected static string Base64Encode(byte[] plainTextBytes)
+        {
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        protected string GetOrderId(SquareWebhookEvent squareEvent)
+        {
+            return squareEvent?.data?._object?.payment?.order_id ?? "";
         }
     }
 }
